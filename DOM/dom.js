e07df55e0da8c69e7f4b331998fbbe417014cf03
stella-mumbi <stella.mumbi@student.moringaschool.com>
2023-12-19 11:23:23 +0300

    DOM

---
+++ DOM/dom.js
@@ -0,0 +1,8 @@
+document.getElementById("add_btn").addEventListener("click", function(event){
+    event.preventDefault()
+
+    let todo = document.getElementById("new_Todo_item").value
+    console.log(todo)
+
+    
+})
